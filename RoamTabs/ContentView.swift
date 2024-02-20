627b3d58eebaf18e177b25e06b92c4924070395c
Forest <forestplasencia@me.com>
2024-02-20 10:32:21 -0800

    Initial Commit

---
+++ RoamTabs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoamTabs
+//
+//  Created by Forest Plasencia on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
